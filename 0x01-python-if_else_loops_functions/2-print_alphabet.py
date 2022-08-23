39ebe9d4203fccef7718637fc6d139b3c5730547
Felixgreat <105022777+Felixgreat@users.noreply.github.com>
2022-08-23 22:04:31 +0100

    P2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-# Felix Greatness <375@holbertonschool.com>
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
