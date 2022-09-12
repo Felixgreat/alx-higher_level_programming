fcff4279b2acdfac463cbc0b86b8b13ea521d241
Felixgreat <105022777+Felixgreat@users.noreply.github.com>
2022-09-12 21:39:38 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    cmpt = 0
-    for index in range(x):
-        try:
-            print('{:d}'.format(my_list[index]), end='')
-            cmpt += 1
-        except IndexError:
-            break
-        except Exception:
-            pass
+# 2-safe_print_list_integers.py
+# Felix Nwaora <375@holbertonschool.com>
 
-    print('')
-    return cmpt
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x elements of a list that are integers.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
