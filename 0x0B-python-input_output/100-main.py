c34ad9f46bfa87779820433f63551d7e0bcf8e7f
Felixgreat <105022777+Felixgreat@users.noreply.github.com>
2022-09-27 13:09:35 +0100

    Update 100-main.py

--- 0x0B-python-input_output/100-main.py
+++ 0x0B-python-input_output/100-main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 append_after = __import__('100-append_after').append_after
 
-append_after("./append_after_100.txt", "Python", "\"C is fun!\"\n")
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
