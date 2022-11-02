93ccaee3bc7f5a967434cd4641d6c09ee15bb097
Felixgreat <105022777+Felixgreat@users.noreply.github.com>
2022-11-02 21:42:52 +0100

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,7 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates user
+-- create user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grant privs
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- flush
 FLUSH PRIVILEGES;
